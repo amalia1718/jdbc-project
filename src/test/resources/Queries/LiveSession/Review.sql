9540c07b4d256c1fb28586f99f99d8928b480166
mehmetCt <mehmet@cybertekschool.com>
2023-07-30 11:45:47 -0400

    Review.sql Tasks are shared

--- src/test/resources/Queries/LiveSession/Review.sql
+++ src/test/resources/Queries/LiveSession/Review.sql
@@ -35,29 +35,82 @@ select * from employees;
 --------------- SELECT ,WHERE, LIKE -------------------
 
 --TASK 1 - display department names  from departments table
+            SELECT  DEPARTMENT_NAME FROM DEPARTMENTS;
+
+            SeLeCT department_name fRoM DEPARTMENTS;
+            -- SQL COMMANDS are case insensitive
+            -- TABLE and COLUMN names are depending database --> HR it is case insensitive
+            -- WHERE FIRSTNAME='' --> '' is case sensitive
 
 --TASK 2 - display first_name, last_name, department_id, salary from employees
+            SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,SALARY FROM EMPLOYEES;
 
 --TASK 3 - display first_name, last_name, department_id, salary from employees whose salary is higher than 9000
+            SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,SALARY FROM EMPLOYEES
+            WHERE SALARY>9000;
+
 
 --TASK 4 - display first_name, last_name, department_id, salary from employees
 --         whose salary equals or more than 9000 and salary equals or less than 15000
 --         sort result based on salary asc
 
+            -- OPT 1
+            SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,SALARY FROM EMPLOYEES
+            WHERE SALARY>= 9000 AND SALARY<=15000
+            ORDER BY SALARY ; -- ASC is DEFAULT OPTION
+            --ORDER BY 4;
+
+            -- USING COLUMN NAME IS BEST PRACTICE
+
+            -- OPT 2
+            SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,SALARY FROM EMPLOYEES
+            WHERE SALARY BETWEEN 9000 AND 15000 --> BETWEEN LOWER AND UPPER (BOUNDRIES ARE INCLUDED)
+            ORDER BY SALARY ;
+
 --TASK 5 -  display first_name, last_name, department_id, salary from employees
 --          whose firstname is Peter and salary is bigger than 5000
 --          sort result based on salary desc
 
+            SELECT FIRST_NAME,LAST_NAME,DEPARTMENT_ID,SALARY FROM EMPLOYEES
+            WHERE FIRST_NAME='Peter' AND SALARY>5000
+            ORDER BY SALARY DESC ;
+            -- 'Peter' needs be case sensitive
+
 --TASK 6 - display country_id, country_name from countries table for region id 2 or 4
 --         sort result based on region_id desc and country_name asc
+
+            -- OPT 1
+            SELECT COUNTRY_ID,COUNTRY_NAME,REGION_ID FROM COUNTRIES
+            WHERE REGION_ID=2 OR REGION_ID=4
+            ORDER BY REGION_ID DESC,COUNTRY_NAME ASC;
+
+            -- OPT 2
+            SELECT COUNTRY_ID,COUNTRY_NAME,REGION_ID FROM COUNTRIES
+            WHERE REGION_ID IN (2,4)
+            ORDER BY REGION_ID DESC,COUNTRY_NAME ASC;
 
 --TASK 7 -  display employee firstname and lastname as fullname , salary as annual salary  , job_id
 --          whose job ID starts with S
 
+            SELECT FIRST_NAME||' '||LAST_NAME as FULLNAME,SALARY*12 as ANNUALSALARY,JOB_ID
+            FROM EMPLOYEES
+            WHERE JOB_ID LIKE 'S%';
+
+            -- % --> REFERS 0 or more letter
+            -- - --> REFERS ONLY ONE LETTER
+            -- CONTAINS --> '%S%'
 
          -- whose job ID starts with S AND TOTAL 6 LETTER
+            SELECT FIRST_NAME||' '||LAST_NAME as FULLNAME,SALARY*12 as ANNUALSALARY,JOB_ID
+            FROM EMPLOYEES
+            WHERE JOB_ID LIKE 'S_____';
+
+
 
 -- TASK 8 - display country id and country name where country name ends with a
+
+
+
 
 -- TASK 9 -  display country id and country name where country name ends with a and third letter is i
 
