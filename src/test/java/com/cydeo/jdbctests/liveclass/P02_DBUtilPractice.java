82f12c99753c40d04393c40122335f297104baed
mehmetCt <mehmet@cybertekschool.com>
2023-08-06 12:38:31 -0400

    Live Class

--- src/test/java/com/cydeo/jdbctests/liveclass/P02_DBUtilPractice.java
+++ src/test/java/com/cydeo/jdbctests/liveclass/P02_DBUtilPractice.java
@@ -1,5 +1,6 @@
 package com.cydeo.jdbctests.liveclass;
 
+import com.cydeo.jdbctests.utility.DB_Util;
 import org.junit.jupiter.api.Test;
 
 public class P02_DBUtilPractice {
@@ -8,31 +9,46 @@ public class P02_DBUtilPractice {
     @Test
     public void test1() {
 
-
+        //CREATE CONN
+        DB_Util.createConnection();
+        //RUN QUERY
+        DB_Util.runQuery("SELECT COUNTRY_ID,COUNTRY_NAME FROM COUNTRIES");
 
         System.out.println("GET ME ROW COUNT ");
+        System.out.println(DB_Util.getRowCount());
 
         System.out.println("GET ME COLUMN COUNT ");
+        System.out.println(DB_Util.getColumnCount());
 
         System.out.println("GET ALL COLUMN NAME AS LIST");
+        System.out.println(DB_Util.getAllColumnNamesAsList());
 
         System.out.println("GET ROW DATA AS LIST--> FIRST ROW --> 1 ");
+        System.out.println(DB_Util.getRowDataAsList(1));
+
 
         System.out.println("GET CELL VALUE --> int rowNum , int columnIndex");
+        System.out.println(DB_Util.getCellValue(1, 1));
 
         System.out.println("GET CELL VALUE --> int rowNum ,String columnName");
+        System.out.println(DB_Util.getCellValue(1, "COUNTRY_ID"));
 
         System.out.println("GET FIRST ROW FIRST COLUMN VALUE");
+        System.out.println(DB_Util.getFirstRowFirstColumn());
 
         System.out.println("GET COLUMN DATA AS LIST--> int columnNum");
+        System.out.println(DB_Util.getColumnDataAsList(2));
 
         System.out.println("GET COLUMN DATA AS LIST--> String columnName");
+        System.out.println(DB_Util.getColumnDataAsList("COUNTRY_NAME"));
+
 
         System.out.println("GET ROW MAP");
 
         System.out.println("GET ALL ROW AS LIST OF MAP");
 
-
+        // CLOSE CONN
+        DB_Util.destroy();
 
     }
 }
